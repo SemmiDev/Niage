c62544b1a546b29786c84ecacb482f558a7f1f0b
Sammi Aldhi Yanto <55814880+SemmiDev@users.noreply.github.com>
2021-03-09 15:07:15 +0700

    customer banyak maunya :D dei

--- src/com/sammidev/pertama/main.c
+++ src/com/sammidev/pertama/main.c
@@ -1,24 +1,21 @@
 #include <stdio.h>
 
 int main(){
-    
-    int lengthBO = 3;
-    int lengthB1 = 5;
 
     char A[15] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O'};
     char B[3][5];
 
-    int index = 0;
+    int n = 0;
 
-    for (int i = 0; i < lengthB1; i++) {
-        for (int j = 0; j < lengthBO; j++) {
-            B[j][i] = A[index];
-            index++;
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 3; j++) {
+            B[j][i] = A[n];
+            n++;
         }
     }
 
-    for (int i = 0; i < lengthBO; i++) {
-            for (int j = 0; j < lengthB1; j++) {
+    for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 5; j++) {
                 printf("%c", B[i][j]);
             }
             printf("\n");
