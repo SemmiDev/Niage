ccae58af2008fe2794a871af48a4460859bcd0b1
Sammi Aldhi Yanto <55814880+SemmiDev@users.noreply.github.com>
2021-03-11 21:29:17 +0700

    Update main.cpp

--- src/com/sammidev/customer4/main.cpp
+++ src/com/sammidev/customer4/main.cpp
@@ -10,6 +10,21 @@ int main(){
     cout<<"Luas persegi adalah "<< luas << endl;
     
     keliling = 4 * s;
-    cout << "Keliling persegi adalah "<< keliling;
+    cout << "Keliling persegi adalah "<< keliling << endl << endl;
+    
+    int panjang,lebar;
+    cout << "Masukan panjang persegi panjang = ";
+    cin >> panjang;
+    
+    cout << "Masukan lebar persegi panjang = ";
+    cin >> lebar;
+    
+    int luasPersegiPanjang = panjang * lebar;
+    int kelilingPersegiPanjang = 2 * (panjang + lebar);
+    
+    cout<<"Luas persegi panjang adalah "<< luasPersegiPanjang << endl;
+    cout<<"Keliling persegi panjang adalah "<< kelilingPersegiPanjang << endl;
+    
+    
     return 0;
 }
