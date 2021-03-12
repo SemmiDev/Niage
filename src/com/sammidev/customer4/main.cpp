b5f268057b58340ed7b95da9d38fc2379198cae6
Sammi Aldhi Yanto <55814880+SemmiDev@users.noreply.github.com>
2021-03-12 22:18:48 +0700

    Update main.cpp

--- src/com/sammidev/customer4/main.cpp
+++ src/com/sammidev/customer4/main.cpp
@@ -1,29 +1,43 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int s,luas,keliling;
+    int s,luas,keliling,pilihan;
 
-    cout << "Masukan sisi persegi = ";
-    cin >> s;
+    cout << "Pilih rumus tersedia:" << endl; 
+    cout << "1. Persegi" << endl; 
+    cout << "2. Persegi Panjang" << endl; 
+    cout << "pilihan anda no = ";
+    cin >> pilihan; 
+
+    if(pilihan == 1){
+       cout << "Masukan sisi persegi = ";
+       cin >> s;
     
-    luas = s*s;
-    cout<<"Luas persegi adalah "<< luas << endl;
+       luas = s*s;
+       cout<<"Luas persegi adalah "<< luas << endl;
     
-    keliling = 4 * s;
-    cout << "Keliling persegi adalah "<< keliling << endl << endl;
+       keliling = 4 * s;
+       cout << "Keliling persegi adalah "<< keliling << endl << endl;     
+    }else if(pilihan == 2) {
+       int panjang,lebar;
+       cout << "Masukan panjang persegi panjang = ";
+       cin >> panjang;
+       
+       cout << "Masukan lebar persegi panjang = ";
+       cin >> lebar;
     
-    int panjang,lebar;
-    cout << "Masukan panjang persegi panjang = ";
-    cin >> panjang;
+       int luasPersegiPanjang = panjang * lebar;
+       int kelilingPersegiPanjang = 2 * (panjang + lebar);
     
-    cout << "Masukan lebar persegi panjang = ";
-    cin >> lebar;
+       cout<<"Luas persegi panjang adalah "<< luasPersegiPanjang << endl;
+       cout<<"Keliling persegi panjang adalah "<< kelilingPersegiPanjang << endl;
     
-    int luasPersegiPanjang = panjang * lebar;
-    int kelilingPersegiPanjang = 2 * (panjang + lebar);
+
+    }else {
+       cout << "pilihan salah";
+    }
+
     
-    cout<<"Luas persegi panjang adalah "<< luasPersegiPanjang << endl;
-    cout<<"Keliling persegi panjang adalah "<< kelilingPersegiPanjang << endl;
     
     
     return 0;
