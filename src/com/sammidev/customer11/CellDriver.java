0d26a6c4b7ca450df0b71a99f587c3916635f5f0
SemmiDev <sammidev4@gmail.com>
2021-03-23 12:15:00 +0700

    one class

--- src/com/sammidev/customer11/CellDriver.java
+++ src/com/sammidev/customer11/CellDriver.java
@@ -11,3 +11,25 @@ public class CellDriver {
         duaTipeB.setT2(true);
     }
 }
+
+class DuaTipe<T1,T2> {
+
+    private T1 t1;
+    private T2 t2;
+
+    public T1 getT1() {
+        return t1;
+    }
+
+    public void setT1(T1 t1) {
+        this.t1 = t1;
+    }
+
+    public T2 getT2() {
+        return t2;
+    }
+
+    public void setT2(T2 t2) {
+        this.t2 = t2;
+    }
+}
