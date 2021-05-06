379f05b74839d557919eb9a56c4c1b3b50945048
SemmiDev <sammidev4@gmail.com>
2021-05-06 11:37:04 +0700

    UP

---
+++ src/tania/no2.py
@@ -0,0 +1,16 @@
+print("\nPROGRAM KONVERSI TEMPERATUR\n")
+
+celcius = float(input('Masukan suhu dalam celcius : '))
+print("suhu adalah",celcius, "Celcius")
+
+# reamur
+reamur = (4/5) * celcius
+print("suhu dalam reamur adalah ",reamur, "Reamur")
+
+# fahrenheit
+fahrenheit = ((9/5) * celcius) + 32
+print("suhu dalam fahrenheit adalah ",fahrenheit, "Fahrenheit")
+
+# Kelvin
+kelvin = celcius + 273
+print("suhu dalam kelvin adalah ",kelvin, "Kelvin")
