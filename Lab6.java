76023184d2a30c024be8d2c7338aad21a1580926
Sammi Aldhi Yanto <55814880+SemmiDev@users.noreply.github.com>
2021-11-16 14:25:51 +0700

    Update Lab6.java

--- Lab6.java
+++ Lab6.java
@@ -17,7 +17,8 @@ class Lab6 {
         out = new PrintWriter(outputStream);
 
         List<Integer> tanah = new ArrayList<Integer>();
-        StringBuilder output = new StringBuilder("\n");
+        List<Integer> temp;
+        StringBuilder output = new StringBuilder();
 
         int N = in.nextInt();
         for (int i = 0; i < N; i++) {
@@ -25,46 +26,60 @@ class Lab6 {
             tanah.add(height);
         }
 
+        int x,y,min,max,indexMin;
         int Q = in.nextInt();
         while(Q-- > 0) {
             String query = in.next();
             if (query.equals("A")) {
-                int y = in.nextInt();
+                y = in.nextInt();
                 tanah.add(y);
             } else if (query.equals("U")) {
-                int x = in.nextInt();
-                int y = in.nextInt();
-                tanah.set(x,y);
-            } else {                
-                int min = Collections.min(tanah);
-                int indexMin = tanah.indexOf(min);
-                int max = 0;
-
-                if (indexMin == 0) {
-                    List<Integer> temp = new ArrayList<Integer>();
-                    temp.add(tanah.get(indexMin));
-                    temp.add(tanah.get(indexMin+1));
-
-                    max = Collections.max(temp);
-                    tanah.set(indexMin, max);
-                    if (indexMin < N-1) {
-                        tanah.set(indexMin + 1, max);
-                    }
+                x = in.nextInt();
+                y = in.nextInt();
+                if (x >= 0 && x < (N-1)) {
+                    tanah.set(x,y);
                 }
-                
-                if (indexMin > 0) {
-                    List<Integer> temp = new ArrayList<Integer>();
-                    temp.add(tanah.get(indexMin-1));
-                    temp.add(tanah.get(indexMin));
-                    temp.add(tanah.get(indexMin+1));
-                    max = Collections.max(temp);
-                    
+            } else {
+                min = Collections.min(tanah);
+                indexMin = tanah.indexOf(min);
+                max = 0;
+
+                int first = -1;
+                int second = -1;
+                int third = -1;
+
+                temp = new ArrayList<Integer>();
+                try {
+                    first = tanah.get(indexMin);
+                }catch (Exception e) {}
+                try {
+                   second = tanah.get(indexMin-1);
+                }catch (Exception e) {}
+                try {
+                   third = tanah.get(indexMin+1);
+                }catch (Exception e) {}
+
+                if (first != -1) {
+                    temp.add(first);
+                }
+                if (second != -1) {
+                    temp.add(second);
+                }
+                if (third != -1) {
+                    temp.add(third);
+                }
+
+                max = Collections.max(temp);
+                if (first != -1) {
+                    tanah.set(indexMin, max);
+                }
+                if (second != -1) {
                     tanah.set(indexMin-1, max);
-                    tanah.set(indexMin, max);
-                    if (indexMin < N-1) {
-                        tanah.set(indexMin + 1, max);
-                    }
                 }
+                if (third != -1) {
+                    tanah.set(indexMin+1, max);
+                }
+
                 output.append(max).append(" ").append(indexMin).append("\n");
             }
         }
